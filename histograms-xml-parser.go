7b8116ce76775e4a2cf731e80b6352007bf07229
Tim Dresser <tdresser@chromium.org>
2017-03-02 16:02:38 -0500

    Fix formatting

--- histograms-xml-parser.go
+++ histograms-xml-parser.go
@@ -6,11 +6,11 @@ import "encoding/xml"
 import "io/ioutil"
 
 type histogram struct {
-	XMLName xml.Name `xml:"histogram"`
-	Name string `xml:"name,attr"`
-  Owner []string `xml:"owner"`
-	Summary string `xml:"summary"`
-	Obsolete string `xml:"obsolete"`
+	XMLName  xml.Name `xml:"histogram"`
+	Name     string   `xml:"name,attr"`
+	Owner    []string `xml:"owner"`
+	Summary  string   `xml:"summary"`
+	Obsolete string   `xml:"obsolete"`
 }
 
 func (histogram histogram) String() string {
@@ -18,17 +18,17 @@ func (histogram histogram) String() string {
 }
 
 type histogramConfiguration struct {
-	XMLName xml.Name `xml:"histogram-configuration"`
+	XMLName    xml.Name    `xml:"histogram-configuration"`
 	Histograms []histogram `xml:"histograms>histogram"`
 }
 
 func main() {
-  file, _ := ioutil.ReadFile(os.Args[1])
+	file, _ := ioutil.ReadFile(os.Args[1])
 
 	var histogramConfiguration histogramConfiguration
 	xml.Unmarshal(file, &histogramConfiguration)
 
-  for _, histogram := range histogramConfiguration.Histograms {
+	for _, histogram := range histogramConfiguration.Histograms {
 		if histogram.Obsolete != "" {
 			continue
 		}
